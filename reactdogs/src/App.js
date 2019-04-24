a3328de400aa02e9f7fae8facb78a68ed68d7136
Kate Beard <asouthernbellein@gmail.com>
2019-04-24 13:56:51 +0100

    Save new app.js

--- reactdogs/src/App.js
+++ reactdogs/src/App.js
@@ -9,30 +9,6 @@ class App extends Component {
   
   render() {
     return (
-<<<<<<< HEAD
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-
-        <p> there will be merge problems! </p>
-
-        <footer>
-          things in here
-        </footer>
-      </div>
-=======
       <div className="App"
           style={{
           position: 'absolute', 
@@ -47,7 +23,6 @@ class App extends Component {
           <MediaCard />
         </Paper>
 </div>
->>>>>>> 126f628c7f1dd121325fe0583e63788b5c813b68
     );
   }
 }
